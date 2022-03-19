879053b51ae01a50645a9213b66947b091a72bdb
Daniel.W <101545435+DanielWiranto@users.noreply.github.com>
2022-03-19 16:00:15 +0800

    Praktikum Pemrograman Mobile

---
+++ Modul1/PRAK101.kt
@@ -0,0 +1,24 @@
+package Modul1
+
+import java.util.*
+
+
+fun main(args: Array<String>){
+
+    val input = Scanner(System.`in`)
+
+    print("Waktu Sekarang : ")
+    val Time = input.nextLine();
+    print("Nama Anda : ")
+    val Name = input.nextLine();
+    print("Umur Anda : ")
+    val Age = input.nextLine();
+    print("Suhu Tubuh Anda Anda : ")
+    val Temp = input.nextLine();
+
+    println()
+    println("Selamat $Time, $Name.\nUmur anda $Age Tahun. \nSuhu Tuhuh anda $Temp derajat \nCelcius.")
+
+
+
+}
