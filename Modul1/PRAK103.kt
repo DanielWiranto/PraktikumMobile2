6a1e658c41e366a1bc138f5c870def684a561677
Daniel.W <101545435+DanielWiranto@users.noreply.github.com>
2022-03-19 17:44:09 +0800

    Looping

---
+++ Modul1/PRAK103.kt
@@ -0,0 +1,26 @@
+package Modul1
+
+import java.util.*
+
+fun main ()
+{
+    val input = Scanner(System.`in`)
+    print("Nilai : ")
+    val nilai = input.nextInt();
+
+    var Jumlah : Int
+    var kali : Int = 0;
+    for(i in 1..999)
+    {
+        Jumlah = nilai*i
+        if(((Jumlah%2)==0)|| ((Jumlah%3)==0)) {
+            print("$Jumlah ")
+
+            kali = kali+1
+        }
+        if(kali == 5){
+            break
+        }
+    }
+
+}
